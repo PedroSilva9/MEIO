ab2f38a3e812520c0940d6c5a63797817d774ace
PedroSilva9 <ojospedro@gmail.com>
2020-05-10 19:47:50 +0100

    Valores de custo a rondar os 800

--- meio.hs
+++ meio.hs
@@ -2,9 +2,10 @@
 import Cp
 import Data.List
 import Data.List.Split (chunksOf)
+import Data.Matrix hiding (transpose)
 
-type Matrix a = [[a]]
-type Pair a = (a,a)
+type Matriz a = [[a]]
+type Par a = (a,a)
 
 f1pp = [0.0488, 0.1244, 0.2208, 0.2176, 0.1868, 0.1036, 0.0596, 0.0244, 0.0104, 0.0028, 0.0008, 0.0000, 0.0000]
 f1pe = [0.0352, 0.0560, 0.1004, 0.1136, 0.1464, 0.1264, 0.1072, 0.1008, 0.0712, 0.0640, 0.0348, 0.0340, 0.0100]
@@ -13,12 +14,12 @@ f2pp = [0.0220, 0.0924, 0.1680, 0.2116, 0.1948, 0.1396, 0.0936, 0.0480, 0.0208, 
 f2pe = [0.0324, 0.0676, 0.1024, 0.1344, 0.1356, 0.1320, 0.0960, 0.0956, 0.0696, 0.0568, 0.0384, 0.0292, 0.0100]
 
 -- gera todas as combinações de estados possiveis
-generateStates :: [Pair Int]
+generateStates :: [Par Int]
 generateStates = [(x,y) | x <- [0..12], y <- [0..12]]
 
 -- gera um par contendo cada um uma lista de pares com os resultados da multiplicação das probabilidades para cada uma das filiais
 -- output : ((filial 1, filial2), (custo1, custo2)) no caso em que nao transfere nada
-filiais :: ((Matrix Double, Matrix Double), (Matrix Double, Matrix Double))
+filiais :: ((Matriz Double, Matriz Double), (Matriz Double, Matriz Double))
 filiais =  devides $ (unzip >< unzip) . unzip . map (\x -> foldr (\l acc -> con acc x l) ((0,0), (0,0)) states) $ states
     where states  = generateStates
           divide  = chunksOf 13
@@ -43,7 +44,7 @@ plus = cond ((<= 12) . uncurry (+)) (uncurry (+)) (const 12)
 minus = cond ((>= 0) . uncurry (-)) (uncurry (-)) (const 0)
 
 -- emparelha as matrizes de ambas as filiais
-allTheMatrices :: [Pair (Matrix Double)]
+allTheMatrices :: [Par (Matriz Double)]
 allTheMatrices = uncurry zip . (func >< func) $ allShifts
     where func input = map (chunksOf 169 . uncurry joinMatrices) $ input
 
@@ -63,7 +64,7 @@ getNonZeroIndexes ll = snd $ foldl (\(i,l) x -> if x/=0 then (i+1,i:l) else (i+1
 -- cria todas as matrizes com os shifts
 -- input : (filial1, filial2)
 -- output : [(filial1, filial2)] -> representa as transações de filial para filial
-generateAllMatrices :: Pair (Matrix Double) -> [Pair (Matrix Double)]
+generateAllMatrices :: Par (Matriz Double) -> [Par (Matriz Double)]
 generateAllMatrices fil = fil : a ++ b
     where (a,b) = (func (upshift >< downshift), func (downshift >< upshift))
           func f = tail . take 4 . iterate f $ fil
@@ -87,16 +88,53 @@ replace li diff lc = chunksOf 13 . snd $ foo
 
 
 -- gera os indices /= 0 para todas as matrizes de cada filial
-allIndexes :: [(Matrix Double, Matrix Double)] -> [([Int], [Int])]
+allIndexes :: [(Matriz Double, Matriz Double)] -> [([Int], [Int])]
 allIndexes = map (getNonZeroIndexes . concat >< getNonZeroIndexes . concat)
 
 -- substrai nas matrizes custo pelo valor pretendido nos indices
 -- pretendidos
--- input:: [([Int], [Int])] -> [(Matrix Double, Matrix Double)]
--- output :: [(Matrix Double)]
+-- input:: [([Int], [Int])] -> [(Matriz Double, Matriz Double)]
+-- output :: [(Matriz Double)]
 subsCosts li lc = snd . foldl(\(acc, l) x -> if (acc < 3)
                                                 then fstTransfers li acc l x
                                                 else sndTransfers li acc l x) (0,[]) $ lc
     where fstTransfers li acc l x = (acc + 1, l ++ [(replace (fst $ (!!) li acc) ((acc + 1) * 7) >< id) $ x])
           sndTransfers li acc l x = (acc + 1, l ++ [(id >< replace (snd $ (!!) li acc) ((acc + 1 - 3) * 7)) $ x])
 
+-- temos um par (probabilidades, custo)
+-- multiplicar por linhas as matrizes
+matrizQ :: Par (Matriz Double) -> Matriz Double
+matrizQ = map (singl . (\y -> foldr (\(a,b) acc -> acc + a*b) 1 y)) . map (uncurry zip) . uncurry zip
+
+matrixMult :: Par (Matriz Double) -> Matriz Double
+matrixMult = toLists . (uncurry multStd) . (fromLists >< fromLists)
+
+matrixAdd :: Matriz Double -> Matriz Double -> Matriz Double
+matrixAdd = zipWith (zipWith (+))
+
+matrixSub :: Matriz Double -> Matriz Double -> Matriz Double
+matrixSub = zipWith (zipWith (-))
+
+matrizQPF :: Matriz Double -> Matriz Double -> Matriz Double -> Matriz Double
+matrizQPF q p f = matrixAdd q $ matrixMult (p, f)
+
+matrizF :: Matriz Double -> Matriz Double
+matrizF = map (singl . maximum) . transpose
+
+-- lista de matrizes QPF
+listMQPF q p f = map (flip (uncurry matrizQPF) f) $ zip q p
+
+recursiva = rec 0.001 (map singl $ replicate 169 0) p q
+    where (p,q) = split (map fst) (map matrizQ) $ allTheMatrices
+
+rec :: Double -> Matriz Double -> [Matriz Double] -> [Matriz Double] -> Matriz Double
+rec n f p q = if foo
+                 then dn calc f
+                 else rec n calc p q
+    where
+          lista = listMQPF q p f
+          calc  = matrizF $ map (map head) $ lista
+          foo   = (< n) . (uncurry (-)) . (split maximum minimum) . concat $ dn calc f
+
+dn :: Matriz Double -> Matriz Double -> Matriz Double
+dn = matrixSub
